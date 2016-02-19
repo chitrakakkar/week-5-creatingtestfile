de644fca53322cc3078cac05f6ec1b50fa69a231
chitrakakkar <India@123>
2016-02-18 18:59:09 -0600

    creating a test file

--- src/com/Chitra/Main.java
+++ src/com/Chitra/Main.java
@@ -12,7 +12,7 @@ public class Main
     public static void main(String[] args) throws Exception
     {
         URL location = Main.class.getProtectionDomain().getCodeSource().getLocation();
-        //System.out.println(location);
+
         File NewFolder = new File("data");
         BufferedWriter output = null;
 
